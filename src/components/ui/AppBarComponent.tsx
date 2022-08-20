7cd009653334fb54e1a42c1e304a0dfbf210eff3
LinZ <linz.webdev@gmail.com>
2022-08-20 09:43:31 -0400

    Menu de opciones

--- src/components/ui/AppBarComponent.tsx
+++ src/components/ui/AppBarComponent.tsx
@@ -1,30 +1,45 @@
-import { FC, useState, MouseEvent } from 'react';
-import { Box, AppBar, Toolbar, IconButton, Typography, Menu, Container, Avatar, Button, Tooltip, MenuItem } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import AdbIcon from '@mui/icons-material/Adb';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { getCookieValue } from '../../lib/functions';
-import { User } from '../../interfaces/user-type';
+import { FC, useState, MouseEvent } from "react";
+import {
+    Box,
+    AppBar,
+    Toolbar,
+    IconButton,
+    Typography,
+    Menu,
+    Container,
+    Avatar,
+    Button,
+    Tooltip,
+    MenuItem,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import AdbIcon from "@mui/icons-material/Adb";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { getCookieValue } from "../../lib/functions";
+import { User } from "../../interfaces/user-type";
 
 type Pages = {
     name: string;
-    path: string
-}
+    path: string;
+};
 type Settings = {
     name: string;
-    path: string
-}
+    path: string;
+};
 
-const pages: Pages[] = [
-    { name: 'Dashboard', path: "/dashboard" }
-];
+const pages: Pages[] = [{ name: "Dashboard", path: "/dashboard" }];
 const settings: Settings[] = [
-    { name: 'Cerrar sesión', path: "/end" }
+    { name: "Administrar usuarios", path: "/admin" },
+    { name: "Procesos y actividades", path: "/process" },
+    { name: "Registrar procesos", path: "/process/add" },
+    { name: "Cerrar sesión", path: "/end" },
 ];
 const adminSettings: Settings[] = [
     { name: "Administrar usuarios", path: "/admin" },
+    { name: "Procesos y actividades", path: "/process" },
+    { name: "Registrar procesos", path: "/process/add" },
     { name: "Cerrar sesión", path: "/end" },
-]
+];
 const notLoggedPages: Pages[] = [
     {
         name: "Log In",
@@ -32,9 +47,9 @@ const notLoggedPages: Pages[] = [
     },
     {
         name: "Registrarse",
-        path: "/register"
-    }
-]
+        path: "/register",
+    },
+];
 
 interface Props {
     title: string;
@@ -67,10 +82,10 @@ export const AppBarComponent: FC<Props> = ({ title, user }) => {
 
     // Direccion actual
     const currentPath = useLocation();
-    console.log({ user })
+    console.log({ user });
     // Render
     return (
-        <AppBar color='secondary' position="static">
+        <AppBar color="secondary" position="static">
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
                     <Typography
@@ -79,18 +94,17 @@ export const AppBarComponent: FC<Props> = ({ title, user }) => {
                         component="a"
                         sx={{
                             mr: 2,
-                            display: { xs: 'none', md: 'flex' },
+                            display: { xs: "none", md: "flex" },
                             fontWeight: 700,
-                            color: 'inherit',
-                            textDecoration: 'none',
+                            color: "inherit",
+                            textDecoration: "none",
                         }}
                     >
                         {title}
                     </Typography>
                     {token && (
                         <>
-
-                            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
                                 <IconButton
                                     size="large"
                                     aria-label="account of current user"
@@ -105,30 +119,36 @@ export const AppBarComponent: FC<Props> = ({ title, user }) => {
                                     id="menu-appbar"
                                     anchorEl={anchorElNav}
                                     anchorOrigin={{
-                                        vertical: 'bottom',
-                                        horizontal: 'left',
+                                        vertical: "bottom",
+                                        horizontal: "left",
                                     }}
                                     keepMounted
                                     transformOrigin={{
-                                        vertical: 'top',
-                                        horizontal: 'left',
+                                        vertical: "top",
+                                        horizontal: "left",
                                     }}
                                     open={Boolean(anchorElNav)}
                                     onClose={handleCloseNavMenu}
                                     sx={{
-                                        display: { xs: 'block', md: 'none' },
+                                        display: { xs: "block", md: "none" },
                                     }}
                                 >
-                                    {pages.map((page) => (
-                                        String(currentPath.pathname) !== String(page.path) && (
-
-                                            <Link style={{ textDecoration: 'none' }} key={page.name} to={page.path}>
-                                                <MenuItem onClick={handleCloseNavMenu}>
-                                                    <Typography textAlign="center">{page.name}</Typography>
-                                                </MenuItem>
-                                            </Link>
-                                        )
-                                    ))}
+                                    {pages.map(
+                                        (page) =>
+                                            String(currentPath.pathname) !== String(page.path) && (
+                                                <Link
+                                                    style={{ textDecoration: "none" }}
+                                                    key={page.name}
+                                                    to={page.path}
+                                                >
+                                                    <MenuItem onClick={handleCloseNavMenu}>
+                                                        <Typography textAlign="center">
+                                                            {page.name}
+                                                        </Typography>
+                                                    </MenuItem>
+                                                </Link>
+                                            )
+                                    )}
                                 </Menu>
                             </Box>
                             <Typography
@@ -136,110 +156,106 @@ export const AppBarComponent: FC<Props> = ({ title, user }) => {
                                 noWrap
                                 component="a"
                                 href=""
-                                sx={{
-                                    mr: 2,
-                                    display: { xs: 'flex', md: 'none' },
-                                    flexGrow: 1,
-                                    fontFamily: 'monospace',
-                                    fontWeight: 700,
-                                    letterSpacing: '.3rem',
-                                    color: 'inherit',
-                                    textDecoration: 'none',
-                                }}
+                                sx={{ mr: 2, display: { xs: "flex", md: "none" }, flexGrow: 1, fontFamily: "monospace", fontWeight: 700, letterSpacing: ".3rem", color: "inherit", textDecoration: "none", }}
                             >
                                 ALTERNOS
                             </Typography>
-                            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                                {pages.map((page) => (
-                                    String(currentPath.pathname) !== String(page.path) && (
-                                        <Button
-                                            key={page.name + 2}
-                                            onClick={() => push(page.path)}
-                                            sx={{ my: 2, color: 'white', display: 'block' }}
-                                        >
-                                            {page.name}
-                                        </Button>
-                                    )))}
+                            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                                {pages.map(
+                                    (page) =>
+                                        String(currentPath.pathname) !== String(page.path) && (
+                                            <Button
+                                                key={page.name + 2}
+                                                onClick={() => push(page.path)}
+                                                sx={{ my: 2, color: "white", display: "block" }}
+                                            >
+                                                {page.name}
+                                            </Button>
+                                        )
+                                )}
                             </Box>
 
                             <Box sx={{ flexGrow: 0 }}>
                                 <Tooltip title="Abrir configuraciones">
                                     <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                        <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                                        <Avatar alt={user?.name || "Nombre de usuario"}>{user?.name.substring(0, 1) || "U"}</Avatar>
                                     </IconButton>
                                 </Tooltip>
-                                <Menu
-                                    sx={{ mt: '45px' }}
-                                    id="menu-appbar"
-                                    anchorEl={anchorElUser}
-                                    anchorOrigin={{
-                                        vertical: 'top',
-                                        horizontal: 'right',
-                                    }}
-                                    keepMounted
-                                    transformOrigin={{
-                                        vertical: 'top',
-                                        horizontal: 'right',
-                                    }}
+                                <Menu sx={{ mt: "45px" }} id="menu-appbar" anchorEl={anchorElUser} anchorOrigin={{ vertical: "top", horizontal: "right", }} keepMounted transformOrigin={{
+                                    vertical: "top", horizontal: "right",
+                                }}
                                     open={Boolean(anchorElUser)}
                                     onClose={handleCloseUserMenu}
                                 >
-                                    {token && (
-                                        (user && user.role_name === "Administrador")
-                                            ? settings.map((setting) => (
-                                                String(currentPath.pathname) !== String(setting.path) && (
-
-                                                    <Link style={{ textDecoration: 'none' }} key={setting.name} to={setting.path}>
+                                    {token &&
+                                        (user && user.role_name === "Administrador"
+                                            ? settings.map(
+                                                (setting) => String(currentPath.pathname) !== String(setting.path) && (
+                                                    <Link style={{ textDecoration: "none" }} key={setting.name} to={setting.path} >
                                                         <MenuItem onClick={handleCloseUserMenu}>
-                                                            <Typography textAlign="center">{setting.name}</Typography>
+                                                            <Typography textAlign="center" color="text.primary">
+                                                                {setting.name}
+                                                            </Typography>
                                                         </MenuItem>
                                                     </Link>
-                                                )))
-                                            : settings.map((setting) => (
-                                                String(currentPath.pathname) !== String(setting.path) && (
-
-                                                    <Link style={{ textDecoration: 'none' }} key={setting.name} to={setting.path}>
-                                                        <MenuItem onClick={handleCloseUserMenu}>
-                                                            <Typography textAlign="center">{setting.name}</Typography>
-                                                        </MenuItem>
-                                                    </Link>
-                                                ))))}
+                                                )
+                                            )
+                                            : settings.map(
+                                                (setting) =>
+                                                    String(currentPath.pathname) !==
+                                                    String(setting.path) && (
+                                                        <Link
+                                                            style={{ textDecoration: "none" }}
+                                                            key={setting.name}
+                                                            to={setting.path}
+                                                        >
+                                                            <MenuItem onClick={handleCloseUserMenu}>
+                                                                <Typography textAlign="center" color="text.primary" >
+                                                                    {setting.name}
+                                                                </Typography>
+                                                            </MenuItem>
+                                                        </Link>
+                                                    )
+                                            ))}
                                 </Menu>
                             </Box>
                         </>
                     )}
-                    {!token && (<>
-                        <Typography
-                            variant="h5"
-                            noWrap
-                            component="a"
-                            href=""
-                            sx={{
-                                mr: 2,
-                                display: { xs: 'flex', md: 'none' },
-                                flexGrow: 1,
-                                fontFamily: 'monospace',
-                                fontWeight: 700,
-                                letterSpacing: '.3rem',
-                                color: 'inherit',
-                                textDecoration: 'none',
-                            }}
-                        >
-                            ALTERNOS
-                        </Typography>
-                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                            {notLoggedPages.map((page) => (
-                                String(currentPath.pathname) !== String(page.path) && (
-                                    <Button
-                                        key={page.name + 2}
-                                        onClick={() => push(page.path)}
-                                        sx={{ my: 2, color: 'white', display: 'block' }}
-                                    >
-                                        {page.name}
-                                    </Button>
-                                )))}
-                        </Box>
-                    </>
+                    {!token && (
+                        <>
+                            <Typography
+                                variant="h5"
+                                noWrap
+                                component="a"
+                                href=""
+                                sx={{
+                                    mr: 2,
+                                    display: { xs: "flex", md: "none" },
+                                    flexGrow: 1,
+                                    fontFamily: "monospace",
+                                    fontWeight: 700,
+                                    letterSpacing: ".3rem",
+                                    color: "inherit",
+                                    textDecoration: "none",
+                                }}
+                            >
+                                ALTERNOS
+                            </Typography>
+                            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                                {notLoggedPages.map(
+                                    (page) =>
+                                        String(currentPath.pathname) !== String(page.path) && (
+                                            <Button
+                                                key={page.name + 2}
+                                                onClick={() => push(page.path)}
+                                                sx={{ my: 2, color: "white", display: "block" }}
+                                            >
+                                                {page.name}
+                                            </Button>
+                                        )
+                                )}
+                            </Box>
+                        </>
                     )}
                 </Toolbar>
             </Container>
